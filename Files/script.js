97361c4bb21b0e7c463212107eb36b6e50f90410
diou <74876641+Diounata@users.noreply.github.com>
2020-12-28 16:53:53 -0300

    update-style

--- Files/script.js
+++ Files/script.js
@@ -1,173 +1,108 @@
-var resultado = [document.getElementById('r1'), document.getElementById('r2'), document.getElementById('r3'), document.getElementById('r4'), document.getElementById('r5'), document.getElementById('r6'), document.getElementById('r7'), document.getElementById('r8'), document.getElementById('r9')]
+var resultado = [document.querySelector('td.r1'), document.querySelector('td.r2'), document.querySelector('td.r3'), document.querySelector('td.r4'), document.querySelector('td.r5'), document.querySelector('td.r6'), document.querySelector('td.r7'), document.querySelector('td.r8'), document.querySelector('td.r9')]
 var casa_preenchida = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 var rodada = [1, 'X', 'O']
 
-function casa1() {
-    if (casa_preenchida[0] == 0) {
+// Preencher casas
+function preencher_casa(n) {
+    if (casa_preenchida[n] == 0) {
         if (rodada[0] == 1) {
-            resultado[0].innerHTML = `${rodada[1]}`
-            casa_preenchida[0] = 1
+            resultado[n].innerHTML = `${rodada[1]}`
+            casa_preenchida[n] = 1
             rodada[0] = 2
         } else {
-            resultado[0].innerHTML = `${rodada[2]}`
-            casa_preenchida[0] = 2
+            resultado[n].innerHTML = `${rodada[2]}`
+            casa_preenchida[n] = 2
             rodada[0] = 1
         }
     }
-
-    verificar_resultado()
 }
 
-function casa2() {
-    if (casa_preenchida[1] == 0) {
-        if (rodada[0] == 1) {
-            resultado[1].innerHTML = `${rodada[1]}`
-            casa_preenchida[1] = 1
-            rodada[0] = 2
-        } else {
-            resultado[1].innerHTML = `${rodada[2]}`
-            casa_preenchida[1] = 2
-            rodada[0] = 1
-        }
-    }
-
-    verificar_resultado()
-}
-
-function casa3() {
-    if (casa_preenchida[2] == 0) {
-        if (rodada[0] == 1) {
-            resultado[2].innerHTML = `${rodada[1]}`
-            casa_preenchida[2] = 1
-            rodada[0] = 2
-        } else {
-            resultado[2].innerHTML = `${rodada[2]}`
-            casa_preenchida[2] = 2
-            rodada[0] = 1
-        }
-    }
-
-    verificar_resultado()
-}
-
-function casa4() {
-    if (casa_preenchida[3] == 0) {
-        if (rodada[0] == 1) {
-            resultado[3].innerHTML = `${rodada[1]}`
-            casa_preenchida[3] = 1
-            rodada[0] = 2
-        } else {
-            resultado[3].innerHTML = `${rodada[2]}`
-            casa_preenchida[3] = 2
-            rodada[0] = 1
-        }
-    }
-
-    verificar_resultado()
-}
-
-function casa5() {
-    if (casa_preenchida[4] == 0) {
-        if (rodada[0] == 1) {
-            resultado[4].innerHTML = `${rodada[1]}`
-            casa_preenchida[4] = 1
-            rodada[0] = 2
-        } else {
-            resultado[4].innerHTML = `${rodada[2]}`
-            casa_preenchida[4] = 2
-            rodada[0] = 1
-        }
-    }
-
-    verificar_resultado()
-}
-
-function casa6() {
-    if (casa_preenchida[5] == 0) {
-        if (rodada[0] == 1) {
-            resultado[5].innerHTML = `${rodada[1]}`
-            casa_preenchida[5] = 1
-            rodada[0] = 2
-        } else {
-            resultado[5].innerHTML = `${rodada[2]}`
-            casa_preenchida[5] = 2
-            rodada[0] = 1
-        }
-    }
-
-    verificar_resultado()
-}
-
-function casa7() {
-    if (casa_preenchida[6] == 0) {
-        if (rodada[0] == 1) {
-            resultado[6].innerHTML = `${rodada[1]}`
-            casa_preenchida[6] = 1
-            rodada[0] = 2
-        } else {
-            resultado[6].innerHTML = `${rodada[2]}`
-            casa_preenchida[6] = 2
-            rodada[0] = 1
-        }
-    }
-
-    verificar_resultado()
-}
-
-function casa8() {
-    if (casa_preenchida[7] == 0) {
-        if (rodada[0] == 1) {
-            resultado[7].innerHTML = `${rodada[1]}`
-            casa_preenchida[7] = 1
-            rodada[0] = 2
-        } else {
-            resultado[7].innerHTML = `${rodada[2]}`
-            casa_preenchida[7] = 2
-            rodada[0] = 1
-        }
-    }
-
-    verificar_resultado()
-}
-
-function casa9() {
-    if (casa_preenchida[8] == 0) {
-        if (rodada[0] == 1) {
-            resultado[8].innerHTML = `${rodada[1]}`
-            casa_preenchida[8] = 1
-            rodada[0] = 2
-        } else {
-            resultado[8].innerHTML = `${rodada[2]}`
-            casa_preenchida[8] = 2
-            rodada[0] = 1
-        }
-    }
-
-    verificar_resultado()
-}
-
-
-var div = document.getElementById('teste')
-
+// Verificar se algum jogador venceu a rodada
 function verificar_resultado() {
     // Verificar eixo x
     if ((casa_preenchida[0] == 1 && casa_preenchida[1] == 1 && casa_preenchida[2] == 1) || (casa_preenchida[3] == 1 && casa_preenchida[4] == 1 && casa_preenchida[5] == 1) || (casa_preenchida[6] == 1 && casa_preenchida[7] == 1 && casa_preenchida[8] == 1)) {
-        div.innerHTML += `Jogador 1 venceu`
+        div_resultado.innerHTML = mensagem_vitoria[0]
+        placar_valor[0] += 1
+        placar_div[0].innerHTML = `(${placar_valor[0]})`
     } else if ((casa_preenchida[0] == 2 && casa_preenchida[1] == 2 && casa_preenchida[2] == 2) || (casa_preenchida[3] == 2 && casa_preenchida[4] == 2 && casa_preenchida[5] == 2) || (casa_preenchida[6] == 2 && casa_preenchida[7] == 2 && casa_preenchida[8] == 2)) {
-        div.innerHTML += `Jogador 2 venceu`
+        div_resultado.innerHTML = mensagem_vitoria[1]
+        placar_valor[1] += 1
+        placar_div[1].innerHTML = `(${placar_valor[1]})`
     }
 
     // Verificar eixo y
     if ((casa_preenchida[0] == 1 && casa_preenchida[3] == 1 && casa_preenchida[6] == 1) || (casa_preenchida[1] == 1 && casa_preenchida[4] == 1 && casa_preenchida[7] == 1) || (casa_preenchida[2] == 1 && casa_preenchida[5] == 1 && casa_preenchida[8] == 1)) {
-        div.innerHTML += `Jogador 1 venceu`
+        div_resultado.innerHTML = mensagem_vitoria[0]
+        placar_valor[0] += 1
+        placar_div[0].innerHTML = `(${placar_valor[0]})`
     } else if ((casa_preenchida[0] == 2 && casa_preenchida[3] == 2 && casa_preenchida[6] == 2) || (casa_preenchida[1] == 2 && casa_preenchida[4] == 2 && casa_preenchida[7] == 2) || (casa_preenchida[2] == 2 && casa_preenchida[5] == 2 && casa_preenchida[8] == 2)) {
-        div.innerHTML += `Jogador 2 venceu`
+        div_resultado.innerHTML = mensagem_vitoria[1]
+        placar_valor[1] += 1
+        placar_div[1].innerHTML = `(${placar_valor[1]})`
     }
 
     // Verificar eixo da diagonal
     if ((casa_preenchida[0] == 1 && casa_preenchida[4] == 1 && casa_preenchida[8] == 1) || (casa_preenchida[2] == 1 && casa_preenchida[4] == 1 && casa_preenchida[6] == 1)) {
-        div.innerHTML += `Jogador 1 venceu`
+        div_resultado.innerHTML = mensagem_vitoria[0]
+        placar_valor[0] += 1
+        placar_div[0].innerHTML = `(${placar_valor[0]})`
     } else if ((casa_preenchida[0] == 2 && casa_preenchida[4] == 2 && casa_preenchida[8] == 2) || (casa_preenchida[2] == 2 && casa_preenchida[4] == 2 && casa_preenchida[6] == 2)) {
-        div.innerHTML += `Jogador 2 venceu`
+        div_resultado.innerHTML = mensagem_vitoria[1]
+        placar_valor[1] += 1
+        placar_div[1].innerHTML = `(${placar_valor[1]})`
     }
 }
+
+// Função para preencher a casa e verificar o resultado
+function casa1() {
+    preencher_casa(0)
+    verificar_resultado()
+}
+
+function casa2() {
+    preencher_casa(1)
+    verificar_resultado()
+}
+
+function casa3() {
+    preencher_casa(2)
+    verificar_resultado()
+}
+
+function casa4() {
+    preencher_casa(3)
+    verificar_resultado()
+}
+
+function casa5() {
+    preencher_casa(4)
+    verificar_resultado()
+}
+
+function casa6() {
+    preencher_casa(5)
+    verificar_resultado()
+}
+
+function casa7() {
+    preencher_casa(6)
+    verificar_resultado()
+}
+
+function casa8() {
+    preencher_casa(7)
+    verificar_resultado()
+}
+
+function casa9() {
+    preencher_casa(8)
+    verificar_resultado()
+}
+
+
+
+// Determinar quem venceu o jogo
+let div_resultado = document.querySelector('div.div-resultado')
+let mensagem_vitoria = ['Jogador 1 venceu', 'Jogador 2 venceu']
+let placar_valor = [0, 0]
+let placar_div = [document.querySelector('span.placar-jogador1'), document.querySelector('span.placar-jogador2')]
