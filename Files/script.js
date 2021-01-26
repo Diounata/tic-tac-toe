e5b52ee044afdaa58d7e7e01676c24c16da000b8
diou <74876641+Diounata@users.noreply.github.com>
2021-01-26 15:57:02 -0300

    Update script.js
    → Recriação do código

--- Files/script.js
+++ Files/script.js
@@ -1,194 +1,179 @@
-var resultado = [document.querySelector('td.r1'), document.querySelector('td.r2'), document.querySelector('td.r3'), document.querySelector('td.r4'), document.querySelector('td.r5'), document.querySelector('td.r6'), document.querySelector('td.r7'), document.querySelector('td.r8'), document.querySelector('td.r9')]
-var casa_preenchida = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-var rodada = [1, 'X', 'O']
-let div_nome_jogadores = [document.querySelector('div.nome_jogador_1'), document.querySelector('div.nome_jogador_2')]
+var casas, casasPreenchidas, rodada, fimRodada, possiveisResultados, divNomeJogadores, divVezJogador, divFimRodada
 
+// + Atribuição de valores de algumas variáveis
+casasPreenchidas = ['', '', '', '', '', '', '', '', '']
+rodada = 'X'
+divVezJogador = document.querySelector('div.divVezJogador')
+divFimRodada = document.querySelector('div.divFimRodada')
+possiveisResultados = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // X
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Y
+    [0, 4, 8], [2, 4, 6]             // D
+]
+casas = [
+    document.querySelector('td.p1'),
+    document.querySelector('td.p2'),
+    document.querySelector('td.p3'),
+    document.querySelector('td.p4'),
+    document.querySelector('td.p5'),
+    document.querySelector('td.p6'),
+    document.querySelector('td.p7'),
+    document.querySelector('td.p8'),
+    document.querySelector('td.p9'),
+]
 
-// Adicionar nome dos jogadores
-function add_jogadores() {
-    sessionStorage.setItem("nome_jogador_1", document.querySelector('input.nome_jogador_1').value)
-    sessionStorage.setItem("nome_jogador_2", document.querySelector('input.nome_jogador_2').value)
+const jogador = {
+    X: {
+        nome: sessionStorage.getItem("nomeJogador_X"),
+        cor: 'text-primary',
+        divPlacar: document.querySelector('span.placarJogador_X'),
+        pontuação: 0,
+    },
+    O: {
+        nome: sessionStorage.getItem("nomeJogador_O"),
+        cor: 'text-danger',
+        divPlacar: document.querySelector('span.placarJogador_O'),
+        pontuação: 0,
+    },
 }
 
-window.addEventListener('load', function add_nick() {
-    vez_jogador.innerHTML = `• Vez de ${sessionStorage.getItem("nome_jogador_1")} (<span class="text-primary mx-1">X</span>)`
-    div_nome_jogadores[0].innerHTML = sessionStorage.getItem("nome_jogador_1")
-    div_nome_jogadores[1].innerHTML = sessionStorage.getItem("nome_jogador_2")
-    placar_div[0].innerHTML = `${placar_valor[0]}`
-    placar_div[1].innerHTML = `${placar_valor[1]}`
-});
+divNomeJogadores = {
+    X: document.querySelector('div.nomeJogador_X'),
+    O: document.querySelector('div.nomeJogador_O'),
+}
+// - Atribuição de valores de algumas variáveis
 
-// Vez do jogador
-var vez_jogador = document.querySelector('div.verificar-vez-jogador')
-function verificar_vez_jogador() {
-    if (rodada[0] == 1) {
-        vez_jogador.innerHTML = `• Vez de ${sessionStorage.getItem("nome_jogador_1")} (<span class="text-primary mx-1">X</span>)`
-    } else {
-        vez_jogador.innerHTML = `• Vez de ${sessionStorage.getItem("nome_jogador_2")} (<span class="text-danger mx-1">O</span>)`
+
+// + Adicionar nome e placar dos jogadores na página
+function addNomes() {
+    sessionStorage.setItem("nomeJogador_X", document.querySelector('input.inputNome_X').value)
+    sessionStorage.setItem("nomeJogador_O", document.querySelector('input.inputNome_O').value)
+}
+
+window.addEventListener('load', () => {
+    for (pos in divNomeJogadores) {
+        divNomeJogadores[pos].innerText = `${jogador[pos].nome}`
+        jogador[pos].divPlacar.innerText = `${jogador[pos].pontuação}`
+    }
+
+    divVezJogador.innerHTML = `• Vez de ${rodada === 'X' ? `${jogador.X.nome} (<span class="${jogador.X.cor} mx-1">X</span>)` : `${jogador.O.nome} (<span class="${jogador.O.cor} mx-1">O</span>)`}`
+});
+// - Adicionar nome dos jogadores na página
+
+
+// + Preencher casas
+fimRodada = false
+function preencherCasa(n) {
+    if (fimRodada === false) {
+        if (casasPreenchidas[n] == '') {
+            if (rodada == 'X') {
+                casas[n].innerHTML = `<span class="${jogador.X.cor}">${rodada}</span>`
+                casasPreenchidas[n] = 'X'
+                rodada = 'O'
+            } else {
+                casas[n].innerHTML = `<span class="${jogador.O.cor}">${rodada}</span>`
+                casasPreenchidas[n] = 'O'
+                rodada = 'X'
+            }
+            divVezJogador.innerHTML = `• Vez de ${rodada === 'X' ? `${jogador.X.nome} (<span class="${jogador.X.cor} mx-1">X</span>)` : `${jogador.O.nome} (<span class="${jogador.O.cor} mx-1">O</span>)`}` // Mudar div informando qual é o jogador que está na sua vez
+        }
     }
 }
+// - Preencher casas
 
 
-// Preencher casas
-let final_rodada = false
-function preencher_casa(n) {
-    if (final_rodada == true) {
-        vez_jogador = ``
-    } else if (final_rodada == false) {
-        if (casa_preenchida[n] == 0) {
-            if (rodada[0] == 1) {
-                resultado[n].innerHTML = `${rodada[1]}`
-                resultado[n].classList.add('text-primary')
-                resultado[n].classList.remove('text-danger')
-                casa_preenchida[n] = 1
-                rodada[0] = 2
-            } else {
-                resultado[n].innerHTML = `${rodada[2]}`
-                resultado[n].classList.add('text-danger')
-                resultado[n].classList.remove('text-primary')
-                casa_preenchida[n] = 2
-                rodada[0] = 1
+// + Verificar se alguém venceu a rodada/empatou
+function verificarResultado(item) {
+    for (let c = 0; c < 2; c++) {
+        let caractere
+        c == 1 ? caractere = 'O' : caractere = 'X'
+        if (fimRodada === false) {
+            if ((casasPreenchidas[item[0]] == caractere) && (casasPreenchidas[item[1]] == caractere) && (casasPreenchidas[item[2]] == caractere)) {
+                fimRodada = true
+                for (pos in casasPreenchidas) {
+                    casasPreenchidas[pos] = ''
+                }
+
+                if (c == 1) {
+                    jogador.O.pontuação++
+                    jogador.O.divPlacar.innerText = `${jogador.O.pontuação}`
+
+                } else {
+                    jogador.X.pontuação++
+                    jogador.X.divPlacar.innerText = `${jogador.X.pontuação}`
+                }
+
+                divVezJogador.innerHTML = ''
+                divFimRodada.innerHTML = `
+                    ${c == 1 ? jogador.O.nome : jogador.X.nome} venceu a rodada <br>
+                    <button class="btn btn-outline-primary rounded-pill my-2" onclick="comecarNovoJogo()">
+                        Começar próxima rodada
+                    </button>
+                `
             }
         }
     }
 }
-
-// Fim da rodada
-let div_resultado = document.querySelector('div.div-resultado')
-let mensagem_vitoria = [
-    `<div class="my-2"> ${sessionStorage.getItem("nome_jogador_1")} venceu a rodada </div>`,
-    `<div class="my-2"> ${sessionStorage.getItem("nome_jogador_2")} venceu a rodada </div>`
-]
-
-let placar_valor = [0, 0]
-let placar_div = [document.querySelector('span.placar-jogador1'), document.querySelector('span.placar-jogador2')]
+// - Verificar se alguém venceu a rodada/empatou
 
 
-function comecar_novo_jogo() {
-    for (t in resultado) {
-        resultado[t].innerHTML = ''
+// + Conteúdo a ser exibido após o fim da rodada
+function comecarNovoJogo() {
+    for (pos in casas) {
+        casas[pos].innerHTML = ''
     }
-    rodada[0] = rodada[0] == 1 ? rodada[0] = 2 : rodada[0] = 1
-    final_rodada = false
-    div_resultado.innerHTML = ``
-    if (rodada[0] == 1) {
-        vez_jogador.innerHTML = `• Vez de ${sessionStorage.getItem("nome_jogador_1")} (<span class="text-primary mx-1">X</span>)`
-    } else {
-        vez_jogador.innerHTML = `• Vez de ${sessionStorage.getItem("nome_jogador_2")} (<span class="text-danger mx-1">O</span>)`
-    }
+
+    fimRodada = false
+    divFimRodada.innerHTML = ''
+    rodada = rodada == 'X' ? rodada = 'O' : rodada = 'X'
+    divVezJogador.innerHTML = `• Vez de ${rodada === 'X' ? `${jogador.X.nome} (<span class="${jogador.X.cor} mx-1">X</span>)` : `${jogador.O.nome} (<span class="${jogador.O.cor} mx-1">O</span>)`}`
 }
-
-function fim_rodada(p) {
-    for (t in casa_preenchida) {
-        casa_preenchida[t] = 0
-    }
-
-    div_resultado.innerHTML = mensagem_vitoria[p]
-    placar_valor[p] += 1
-    placar_div[p].innerHTML = `${placar_valor[p]}`
-    div_resultado.innerHTML += `
-        <button href="index.html" class="btn btn-outline-primary rounded-pill" onclick="comecar_novo_jogo()"> Começar próxima rodada </button>
-    `
-    vez_jogador.innerHTML = ``
-    final_rodada = true
-}
-
-function vitoria_x() {
-    fim_rodada(0)
-}
-
-function vitoria_o() {
-    fim_rodada(1)
-}
-
-// Verificar se algum jogador venceu a rodada
-function verificar_resultado() {
-    // Verificar eixo x
-    if ((casa_preenchida[0] == 1 && casa_preenchida[1] == 1 && casa_preenchida[2] == 1) || (casa_preenchida[3] == 1 && casa_preenchida[4] == 1 && casa_preenchida[5] == 1) || (casa_preenchida[6] == 1 && casa_preenchida[7] == 1 && casa_preenchida[8] == 1)) {
-        vitoria_x()
-    } else if ((casa_preenchida[0] == 2 && casa_preenchida[1] == 2 && casa_preenchida[2] == 2) || (casa_preenchida[3] == 2 && casa_preenchida[4] == 2 && casa_preenchida[5] == 2) || (casa_preenchida[6] == 2 && casa_preenchida[7] == 2 && casa_preenchida[8] == 2)) {
-        vitoria_o()
-    }
-
-    // Verificar eixo y
-    if ((casa_preenchida[0] == 1 && casa_preenchida[3] == 1 && casa_preenchida[6] == 1) || (casa_preenchida[1] == 1 && casa_preenchida[4] == 1 && casa_preenchida[7] == 1) || (casa_preenchida[2] == 1 && casa_preenchida[5] == 1 && casa_preenchida[8] == 1)) {
-        vitoria_x()
-    } else if ((casa_preenchida[0] == 2 && casa_preenchida[3] == 2 && casa_preenchida[6] == 2) || (casa_preenchida[1] == 2 && casa_preenchida[4] == 2 && casa_preenchida[7] == 2) || (casa_preenchida[2] == 2 && casa_preenchida[5] == 2 && casa_preenchida[8] == 2)) {
-        vitoria_o()
-    }
-
-    // Verificar eixo da diagonal
-    if ((casa_preenchida[0] == 1 && casa_preenchida[4] == 1 && casa_preenchida[8] == 1) || (casa_preenchida[2] == 1 && casa_preenchida[4] == 1 && casa_preenchida[6] == 1)) {
-        vitoria_x()
-    } else if ((casa_preenchida[0] == 2 && casa_preenchida[4] == 2 && casa_preenchida[8] == 2) || (casa_preenchida[2] == 2 && casa_preenchida[4] == 2 && casa_preenchida[6] == 2)) {
-        vitoria_o()
-    }
-
-    // Verificar se o jogo empatou
-    for (t = 0; casa_preenchida[t] > 0; t++) {
-        if (t == 8) {
-            div_resultado.innerHTML += `
-                Empate <br>
-                <button href="index.html" class="btn btn-outline-primary rounded-pill" onclick="comecar_novo_jogo()"> Começar próxima rodada </button>
-            `
-            vez_jogador.innerHTML = ``
-            final_rodada = true
-        }
-    }
-}
+// - Conteúdo a ser exibido no fim da rodada
 
 
-// Funções para preencher a casa, verificar a vez do jogador e o resultado
+// + Funções para casas
 function casa1() {
-    preencher_casa(0)
-    verificar_vez_jogador()
-    verificar_resultado()
+    preencherCasa(0)
+    possiveisResultados.forEach(verificarResultado)
 }
 
 function casa2() {
-    preencher_casa(1)
-    verificar_vez_jogador()
-    verificar_resultado()
+    preencherCasa(1)
+    possiveisResultados.forEach(verificarResultado)
 }
 
 function casa3() {
-    preencher_casa(2)
-    verificar_vez_jogador()
-    verificar_resultado()
+    preencherCasa(2)
+    possiveisResultados.forEach(verificarResultado)
 }
 
 function casa4() {
-    preencher_casa(3)
-    verificar_vez_jogador()
-    verificar_resultado()
+    preencherCasa(3)
+    possiveisResultados.forEach(verificarResultado)
 }
 
 function casa5() {
-    preencher_casa(4)
-    verificar_vez_jogador()
-    verificar_resultado()
+    preencherCasa(4)
+    possiveisResultados.forEach(verificarResultado)
 }
 
 function casa6() {
-    preencher_casa(5)
-    verificar_vez_jogador()
-    verificar_resultado()
+    preencherCasa(5)
+    possiveisResultados.forEach(verificarResultado)
 }
 
 function casa7() {
-    preencher_casa(6)
-    verificar_vez_jogador()
-    verificar_resultado()
+    preencherCasa(6)
+    possiveisResultados.forEach(verificarResultado)
 }
 
 function casa8() {
-    preencher_casa(7)
-    verificar_vez_jogador()
-    verificar_resultado()
+    preencherCasa(7)
+    possiveisResultados.forEach(verificarResultado)
 }
 
 function casa9() {
-    preencher_casa(8)
-    verificar_vez_jogador()
-    verificar_resultado()
+    preencherCasa(8)
+    possiveisResultados.forEach(verificarResultado)
 }
+// - Funções para casas
