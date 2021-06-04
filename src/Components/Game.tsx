52d2f9aefbb4d3a20249df100ee2295ee2a36496
diou <74876641+Diounata@users.noreply.github.com>
2021-06-04 20:56:04 -0300

    Create game table

--- src/Components/Game.tsx
+++ src/Components/Game.tsx
@@ -1,7 +1,27 @@
+import styles from '../styles/Game.module.scss';
+
 export default function Game() {
     return (
-        <table>
-            <tbody></tbody>
+        <table className={styles.gameContainer}>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
         </table>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Scoreboard from '../Components/Scoreboard';
+import Game from '../Components/Game';
 
 export default function Home() {
     return (
         <div className='container'>
             <Scoreboard />
+            <Game />
         </div>
     );
 }
