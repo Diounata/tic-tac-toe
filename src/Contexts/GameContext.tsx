c0104132009a0d20bdcc7e9cb58c3dd76217eb3e
diou <74876641+Diounata@users.noreply.github.com>
2021-06-09 19:30:36 -0300

    Update GameContext

--- src/Contexts/GameContext.tsx
+++ src/Contexts/GameContext.tsx
@@ -20,22 +20,43 @@ interface ContextProps {
 
 export function GameContextProvider({ children }: ChildrenProps) {
     const [position, setPosition] = useState(['','','','','','','','','']);
-    const [playerTurn, setPlayerTurn] = useState('Player'); // Will start as X
+    const [playerTurn, setPlayerTurn] = useState('Player'); // It'll start as X
+    const [amountFilledPosition, setAmountFilledPosition] = useState(0);
 
     function changePosition(number: number): void {
         if (!position[number]) {
-            const newPosition = position.map((i, index) => number === index ? playerTurn : i);
+            const newPosition = position.map((i, index) =>
+                number === index ? playerTurn : i
+            );
 
             setPosition(newPosition);
+            setAmountFilledPosition(() => amountFilledPosition + 1);
         }
     }
 
-    function verifyIfGameHasFinished(): void {
-        const combinations = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]];
+    function checkGameSituation() {
+        if (amountFilledPosition === 9) {
+            checkIfGameHasTie();
+        } else {
+            checkIfGameHasFinished();
+        }
+    }
+
+    function checkIfGameHasFinished(): void {
+        const combinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
 
         combinations.forEach(pos => {
             let c = 0;
-            
+
             pos.forEach(n => {
                 if (position[n] === playerTurn) {
                     c++;
@@ -46,9 +67,13 @@ export function GameContextProvider({ children }: ChildrenProps) {
         });
     }
 
+    function checkIfGameHasTie(): void {
+        alert('The game has tie.');
+    }
+
     useEffect(() => {
         if (playerTurn === 'X' || playerTurn === 'O') {
-            verifyIfGameHasFinished();
+            checkGameSituation();
         }
 
         setPlayerTurn(playerTurn === 'X' ? 'O' : 'X');
@@ -65,7 +90,7 @@ export function GameContextProvider({ children }: ChildrenProps) {
             {children}
         </GameContext.Provider>
     );
-        }
+}
 
 export function useGame() {
     return useContext(GameContext);
