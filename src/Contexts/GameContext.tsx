82275275c9da90f503d6f743495bc76387f1c104
diou <74876641+Diounata@users.noreply.github.com>
2021-07-28 21:29:24 -0300

    Fix: infinite state cycle

--- src/Contexts/GameContext.tsx
+++ src/Contexts/GameContext.tsx
@@ -244,8 +244,6 @@ export function GameContextProvider({ children }: ChildrenProps) {
         setPlayersName(usernames);
     }, [player]);
 
-    useEffect(() => resetGame());
-
     return (
         <GameContext.Provider
             value={{
