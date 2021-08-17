d0e7da8874836923e84ace226c90361317988b5c
diou <74876641+Diounata@users.noreply.github.com>
2021-08-17 20:52:52 -0300

    Change: Random to Evenly (fail)

--- src/Components/Settings/StartGameAs.tsx
+++ src/Components/Settings/StartGameAs.tsx
@@ -46,7 +46,7 @@ export default function StartGameAs() {
                 selected={startGameAs === 'Evenly'}
                 onClick={() => updateStartGameAs('Evenly')}
             >
-                Random
+                Evenly
             </RadioButton>
 
             <RadioButton
