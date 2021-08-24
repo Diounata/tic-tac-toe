7a569d1d0635f2b1e9864737c2f284b247cc4b58
Diounata <jonathamcordeiro42@gmail.com>
2021-08-24 20:24:14 -0300

    Update: History

--- src/Components/History/HistoryData.tsx
+++ src/Components/History/HistoryData.tsx
@@ -12,24 +12,38 @@ export default function HistoryData() {
     const { history } = usePlayers();
 
     return (
-        <div className={styles.historyItemContainer}>
+        <>
             {history.map((i, key) => (
                 <div className={styles.historyItem} key={key}>
-                    <div className={styles.username}>
-                        <X color={i.x.color} /> {i.x.name}
-                    </div>
-                    <div className={styles.situation}>
-                        <ColorSquare situation={i.x.situation} /> {i.x.situation}
-                    </div>
-                    <div className={styles.username}>
-                        <O color={i.o.color} /> {i.o.name}
-                    </div>
-                    <div className={styles.situation}>
-                        <ColorSquare situation={i.o.situation} /> {i.o.situation}
-                    </div>
-                    <div>{formatTime(i.duration)}</div>
+                    <header>
+                        <span>#{key + 1}</span>
+
+                        <small>{formatTime(i.duration)}</small>
+                    </header>
+
+                    <main>
+                        <article>
+                            <div className={styles.username}>
+                                <X color={i.x.color} /> {i.x.name}
+                            </div>
+
+                            <div className={styles.situation}>
+                                {i.x.situation} <ColorSquare situation={i.x.situation} />
+                            </div>
+                        </article>
+
+                        <article>
+                            <div className={styles.username}>
+                                <O color={i.o.color} /> {i.o.name}
+                            </div>
+
+                            <div className={styles.situation}>
+                                {i.o.situation} <ColorSquare situation={i.o.situation} />
+                            </div>
+                        </article>
+                    </main>
                 </div>
             ))}
-        </div>
+        </>
     );
 }
--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -3,13 +3,9 @@ import styles from '@styles/History/History.module.scss';
 import TitlePage from '@utils/TitlePage';
 import BackButton from '@Components/General/BackButton';
 import HistoryIcon from '@Icons/History';
-import Trash from '@Icons/Trash';
 import ExclamationTriangle from '@Icons/ExclamationTriangle';
-import X from '@Icons/X';
-import O from '@Icons/O';
 
 import Header from '@Components/General/Header';
-import Button from '@Components/General/Button';
 import HistoryData from '@Components/History/HistoryData';
 import Footer from '@Components/General/Footer';
 import Alert from '@Components/General/Alert';
@@ -18,7 +14,6 @@ import { usePlayers } from '@Contexts/PlayersContext';
 import { useSettings } from '@Contexts/SettingsContext';
 
 export default function History() {
-    const { deleteAllHistory } = usePlayers();
     const { isSaveGameStatsOn } = useSettings();
 
     return (
@@ -44,30 +39,6 @@ export default function History() {
                 )}
 
                 <div className={styles.container}>
-                    <div className={styles.tableHeader}>
-                        <div className={styles.username}>
-                            <X /> Player
-                        </div>
-
-                        <div>Situation</div>
-
-                        <div className={styles.username}>
-                            <O /> Player
-                        </div>
-
-                        <div>Situation</div>
-
-                        <div>
-                            Duration
-                        </div>
-
-                        <div>
-                            <Button onClick={deleteAllHistory}>
-                                <Trash /> Delete all
-                            </Button>
-                        </div>
-                    </div>
-
                     <HistoryData />
                 </div>
             </main>
