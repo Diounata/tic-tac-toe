0fa456031dc57f7849dbc61d8c78b94418aae10a
jonathamcordeiro42@gmail.com <jonathamcordeiro42@gmail.com>
2021-09-20 20:24:39 -0300

    Add: PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,15 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 export default class MyDocument extends Document {
     render() {
         return (
-            <Html>
+            <Html lang="en">
                 <Head>
-                    <link rel='shortcut icon' href='./favicon.svg' type='image/x-icon' />
-                    <link rel='preconnect' href='https://fonts.gstatic.com' />
-                    <link href='https://fonts.googleapis.com/css2?family=Inter&display=swap' rel='stylesheet' />
+                    <link rel="shortcut icon" href="./favicon.svg" type="image/x-icon" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="apple-touch-icon" href="/favicon.svg" />
+                    <meta name="theme-color" content="#E1E1E1" />
                 </Head>
 
                 <body>
